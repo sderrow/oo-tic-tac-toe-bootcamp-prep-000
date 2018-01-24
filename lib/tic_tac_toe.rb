87ba159eaf4e4ca2c8e2b1ef6f9e063acd3a150f
Sean Derrow <sderrow@mit.edu>
2018-01-24 21:20:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,6 +34,18 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+  
 end
 
 def won?(board)
@@ -71,18 +83,6 @@ end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
 end
 
 def play(board)
