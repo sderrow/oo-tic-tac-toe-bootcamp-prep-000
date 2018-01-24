78d5dfc73f125f6eb44fa9d38fbdf7b4b05c11b1
Sean Derrow <sderrow@mit.edu>
2018-01-24 21:22:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,8 @@ class TicTacToe
     !(@board[index].nil? || @board[index] == " ")
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && !self.position_taken?(index)
   end
 
   def turn
