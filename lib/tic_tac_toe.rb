64b98292c0d8ae24ae5d60f1c3d7e3933e8e8e9e
Sean Derrow <sderrow@mit.edu>
2018-01-24 21:18:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,14 @@ class TicTacToe
     [2, 4, 6]
   ]
 
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
 end
 
 # Helper Method
@@ -52,14 +60,6 @@ end
 
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
-end
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(user_input)
